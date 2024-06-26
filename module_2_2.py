b1d8b4da2823f2fa8e54b2444625b08bb1981f82
Kos726 <konstantin.e.mikhaylov@gmail.com>
2024-06-26 20:47:11 +0300

    На проверку

--- module_2_2.py
+++ module_2_2.py
@@ -9,4 +9,4 @@ if first == second and second == third:
 elif first == second or first == third or second == third:
     print(2, 'Да, два. Нет это не оценка, это просто значение...')
 else:
-    print(0, 'Ноль, он и в Африке ноль. Почему именно тут ноль? Почем я знаю!')
+    print(0, 'Ноль, он и в Африке ноль. Почему именно тут ноль? Почем я знаю!!!')
