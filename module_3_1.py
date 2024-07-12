5c3c7b75ce59607ed85bf198317cf9d699137c6b
Kos726 <konstantin.e.mikhaylov@gmail.com>
2024-07-13 01:21:02 +0300

    На проверку

--- module_3_1.py
+++ module_3_1.py
@@ -1,21 +1,17 @@
+# Вариант 1
 calls = 0
+def counts_calls_1():
+    global calls                                    # задаем глобальную функцию
+    calls += 1                                      # подсчет количества обращений
 
-# В задании написано создать отдельную функцию counts_calls.
-# Зачем она нужна я так и не разобрался.
-# Потомучто счетчик работает и без нее - см. ответ по Варианту 1. Или я чего-то не понимаю.
-# Однако, я сделал также вариант и с ней - см. ответ по Варинату 2.
-# Но чтоб отделить глобальную переменную варианта 1 от варианта 2,
-# ввожу новую "локальную" переменную calls_, и считаю по ней отдельно.
-
-def counts_calls():
-    global calls                                    # импортируем глобальную функцию
-    calls_ = 0                                      # новая переменная для подсчета обращений варианта 2
-    calls_ = calls_ + calls                         # подсчет количества обращений
-    return calls_
+# Вариант 2
+def counts_calls_2():
+    counts_calls_2.counter += 1                     # используем оператор подсчета
+counts_calls_2.counter = 0                          # стартовое значение подсчета
 
 def string_info(string):
-    global calls                                    # импортируем глобальную функцию
-    calls += 1                                      # прибавляем к calls единицу при работе функции
+    counts_calls_1()                                # вызываем функцию подсчета обращений варианта 1
+    counts_calls_2()                                # вызываем функцию подсчета обращений варианта 2
     string_len = len(string)                        # подсчет символов
     string_up = string.upper()                      # повышение регистра
     string_low = string.lower()                     # понижение регистра
@@ -23,8 +19,8 @@ def string_info(string):
     return tuple_
 
 def is_contains(string, list_to_search):
-    global calls                                    # импортируем глобальную переменную
-    calls += 1                                      # прибавляем к calls единицу при работе функции
+    counts_calls_1()                                # вызываем функцию подсчета обращений варианта 1
+    counts_calls_2()                                # вызываем функцию подсчета обращений варианта 2
     string = string.lower()                         # приводим к нижнему регистру строку в string
     i = 0                                           # начальное условие для количества элементов в list_to_search
     list_ = []                                      # создаем пустой список
@@ -39,5 +35,6 @@ print(string_info('Capybara'))
 print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
 print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
-print(f' Вариант 1 : {calls}')                                  # вывод варианта 1
-print(f' Вариант 2 : {counts_calls()}')                         # вывод варианта 2
+print(f' Ответ по варианту подсчета 1: {calls}')
+print(f' Ответ по варианту подсчета 2: {counts_calls_2.counter}')
+
