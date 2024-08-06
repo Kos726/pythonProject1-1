3c42cab63e6737b184c0cd516acf7c8be765bede
Kos726 <konstantin.e.mikhaylov@gmail.com>
2024-08-07 02:03:48 +0300

    На проверку

---
+++ datas.py
@@ -0,0 +1,19 @@
+print(5)
+print(type(5))   # int - integer - целое
+print(5 + 5)
+print(5 // 5)  # // - целочисленное деление
+print(5 / 5)   # /  - деление с остатком = float
+print(5 % 5)   # % - нахождение остатка от деления, используется для определения четности при делении на 2
+print(5 ** 2)   # ** - возвдение в степень
+print(type(2.0))
+print('Hello, World!')
+print(type('Hello, World!'))  # str - string = строка
+print('Hello, ' + 'World!')  # concatenate - сложение
+print(True, False)    #  - Булевы операции, логический тип данных
+print(type(True), type(False))
+print(5 > 10, 10 > 5, 5 >= 5, 5 <= 5, 5 == 5, 5 != 5)
+print(5 != 5 and 5 < 10, 5 != 5 or 5 < 10)
+print(int('5'))
+print(type(int('5')))
+print(float('5'))
+print(type(float('5')))
