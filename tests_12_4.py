ec532d81cc28d1d54d5db768b61860cf60613f2e
Kos726 <konstantin.e.mikhaylov@gmail.com>
2024-10-24 23:18:41 +0300

    На проверку

---
+++ tests_12_4.py
@@ -0,0 +1,42 @@
+import unittest
+import logging
+import rt_with_exceptions
+
+
+class RunnerTest(unittest.TestCase):
+
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_walk(self):
+        test_volume = 50
+        test_name = "NoName_1"
+        import_runner_ = rt_with_exceptions.Runner(test_name)
+        test_walk_ = [import_runner_.walk() for i in range(10)]
+        self.assertEqual(import_runner_.distance, test_volume)  # 1-функ., кот. тестим, 2-с чем сравниваем
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_run(self):
+        test_volume = 100
+        test_name = "NoName_2"
+        import_runner_ = rt_with_exceptions.Runner(test_name)
+        test_run_ = [import_runner_.run() for i in range(10)]
+        self.assertEqual(import_runner_.distance, test_volume)  # 1-функ., кот. тестим, 2-с чем сравниваем
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_challenge(self):
+        test_name_1 = "NoName_1"
+        test_name_2 = "NoName_2"
+        import_runner_1 = rt_with_exceptions.Runner(test_name_1)
+        import_runner_2 = rt_with_exceptions.Runner(test_name_2)
+        test_walk_ = [import_runner_1.walk() for i in range(10)]
+        test_run_ = [import_runner_2.run() for i in range(10)]
+        self.assertNotEqual(import_runner_1.distance, import_runner_2.distance)
+
+
+if __name__ == '__main__':
+    logging.debug('gf')
+    logging.info('f')
+    logging.warning('g')
+    logging.error('d')
+    logging.critical('a')
