38ddd4400401edbd4445763756d714077c19484c
Kos726 <konstantin.e.mikhaylov@gmail.com>
2024-10-25 19:10:24 +0300

    tests_12_4.py

--- tests_12_4.py
+++ tests_12_4.py
@@ -9,19 +9,31 @@ class RunnerTest(unittest.TestCase):
 
     @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
     def test_walk(self):
-        test_volume = 50
-        test_name = "NoName_1"
-        import_runner_ = rt_with_exceptions.Runner(test_name)
-        test_walk_ = [import_runner_.walk() for i in range(10)]
-        self.assertEqual(import_runner_.distance, test_volume)  # 1-функ., кот. тестим, 2-с чем сравниваем
+        print('test_walk запущен')
+        try:
+        	test_volume = 50
+        	test_name = "NoName_1"
+        	test_speed = 5
+        	import_runner_ = rt_with_exceptions.Runner(test_name, test_speed)
+        	test_walk_ = [import_runner_.walk() for i in range(10)]
+        	self.assertEqual(import_runner_.distance, test_volume)  # 1-функ., кот. тестим, 2-с чем сравниваем
+        	logging.info('"test_walk"выполнен успешно')
+        except ValueError as err:
+        	logging.warning("Неверная скорость для Runner", exc_info=True)
 
     @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
     def test_run(self):
+        print('test_run запущен')
         test_volume = 100
-        test_name = "NoName_2"
-        import_runner_ = rt_with_exceptions.Runner(test_name)
-        test_run_ = [import_runner_.run() for i in range(10)]
-        self.assertEqual(import_runner_.distance, test_volume)  # 1-функ., кот. тестим, 2-с чем сравниваем
+        #test_name = "NoName_2"
+    	test_name = [3]
+        try:
+        	import_runner_ = rt_with_exceptions.Runner(test_name)
+        	test_run_ = [import_runner_.run() for i in range(10)]
+        	self.assertEqual(import_runner_.distance, test_volume)  # 1-функ., кот. тестим, 2-с чем сравниваем
+        	logging.info('"test_run"выполнен успешно')
+        except TypeError as err:
+        	logging.debug("Неверная скорость для Runner", exc_info=True)
 
     @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
     def test_challenge(self):
@@ -33,10 +45,6 @@ class RunnerTest(unittest.TestCase):
         test_run_ = [import_runner_2.run() for i in range(10)]
         self.assertNotEqual(import_runner_1.distance, import_runner_2.distance)
 
-
 if __name__ == '__main__':
-    logging.debug('gf')
-    logging.info('f')
-    logging.warning('g')
-    logging.error('d')
-    logging.critical('a')
+	logging.basicConfig(level=logging.INFO, filemode='w', filename= "_test_log.log", format="%(asctime)s | %(levelname)s | %(message)s")
+	unittest.main()
