7ec15dc4cfc4c1555611e409493cdf7bf47c118e
Kos726 <konstantin.e.mikhaylov@gmail.com>
2024-11-04 19:26:04 +0300

    На проверку

--- Module_14_database/crud_functions.py
+++ Module_14_database/crud_functions.py
@@ -1,10 +1,11 @@
 import sqlite3
 
-list_products_file_name = "products.db"
+list_products_file_name = "database_module_14_bot.db"
+list_users_file_name = "database_module_14_bot.db"
 
 
-def initiate_db(name_=list_products_file_name):
-	connection = sqlite3.connect(name_)
+def initiate_db():
+	connection = sqlite3.connect(list_products_file_name)
 	cursor = connection.cursor()
 
 	# В таблице  будем использовать ссылку к картинкам вместо BLOB
@@ -22,11 +23,31 @@ def initiate_db(name_=list_products_file_name):
 
 	connection.commit()
 	connection.close()
-	print('Таблица создана')
+	print('Таблица products создана')
+
+	connection = sqlite3.connect(list_users_file_name)
+	cursor = connection.cursor()
+
+	# В таблице  будем использовать ссылку к картинкам вместо BLOB
+	cursor.execute('''
+					CREATE TABLE IF NOT EXISTS Users(
+					id INTEGER PRIMARY KEY,
+					username TEXT NOT NULL,
+					email TEXT,
+					age INTEGER NOT NULL,
+					balance INTEGER NOT NULL   
+					)
+					''')
+
+	cursor.execute("CREATE INDEX IF NOT EXISTS idx_username ON Users (username)")
+
+	connection.commit()
+	connection.close()
+	print('Таблица users создана')
 
 
-def get_all_products(id_product_number=0, local_product=False, name_=list_products_file_name):
-	connection = sqlite3.connect(name_)
+def get_all_products(id_product_number=0, local_product=False):
+	connection = sqlite3.connect(list_products_file_name)
 	cursor = connection.cursor()
 
 	# Подсчет общего количества продуктов
@@ -39,7 +60,8 @@ def get_all_products(id_product_number=0, local_product=False, name_=list_produc
 
 		elif local_product is True:
 			cursor.execute("SELECT title, description, price, photo FROM Products WHERE id = ?",
-						(id_product_number + 1, ))
+						(id_product_number + 1, )
+						)
 			params_product = cursor.fetchone()
 			product = [param for param in params_product]
 			print(product)
@@ -49,6 +71,31 @@ def get_all_products(id_product_number=0, local_product=False, name_=list_produc
 
 	connection.commit()
 	connection.close()
+
+
+def add_users(username, email, age):
+	connection = sqlite3.connect(list_users_file_name)
+	cursor = connection.cursor()
+	cursor.execute(
+		"INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+		(username, email, age, 1000)
+		)
+	connection.commit()
+	connection.close()
+
+
+def is_included(username):
+	connection = sqlite3.connect(list_users_file_name)
+	cursor = connection.cursor()
+	check_user = cursor.execute("SELECT username FROM Users WHERE username = ?",
+								(username, ))
+	if check_user.fetchone() is not None:
+		check = True
+	else:
+		check = False
+	connection.commit()
+	connection.close()
+	return check
 
 
 if __name__ == '__main__':
