1209557b1e31ecdffc58d2a794999041ffbef5fd
Kos726 <konstantin.e.mikhaylov@gmail.com>
2024-11-25 21:45:51 +0300

    На проверку

--- Module_16_/module_16_1.py
+++ Module_16_/module_16_1.py
@@ -4,20 +4,20 @@ app = FastAPI()
 
 
 @app.get("/")
-async def main_page() -> dict:
-    return {"message": 'Главная страница'}
+async def main_page() -> str:
+    return f'Главная страница'
 
 
 @app.get("/user/admin")
-async def admin_user() -> dict:
-    return {"message": 'Вы вошли как администратор'}
+async def admin_user() -> str:
+    return f'Вы вошли как администратор'
 
 
 @app.get("/user/{user_id}")
-async def id_enter(user_id: int) -> dict:
-    return {"message": f'Вы вошли как пользователь №{user_id}'}
+async def id_enter(user_id: int) -> str:
+    return f'Вы вошли как пользователь №{user_id}'
 
 
 @app.get("/user")
-async def old_user(user_name: str, age: int) -> dict:
-    return {"message": f'Информация о пользователе. Имя: {user_name}, Возраст: {age}'}
+async def old_user(user_name: str, age: int) -> str:
+    return f'Информация о пользователе. Имя: {user_name}, Возраст: {age}'
